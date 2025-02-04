58f4eb702439834b2a1e6a113ae516d96c0a2064
Rajat Sharma <sharmarajat687@gmail.com>
2025-02-04 23:45:15 +0530

    User table created

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express, { Express, Request, Response } from "express"
 const app: Express = express();
+import { PORT } from "./secrets"
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello World");
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("Server is running on port 3000");
 });
